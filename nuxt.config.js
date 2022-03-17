56368dd14f537a58e7f8cc0ecb5ea233bc928fe2
Yoann Gendrey <ygendrey@gmail.com>
2022-03-17 19:18:34 +0100

    fix: analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['nuxt-buefy', '@nuxtjs/axios', '@nuxtjs/google-adsense'],
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
